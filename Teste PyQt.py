1e18c0be008993c1daa8aa4012b81f05e4adfa96
warwintryborn <felipe.simao@evertical.local>
2016-10-06 11:49:19 -0300

    Teste do PyQT

---
+++ Teste PyQt.py
@@ -0,0 +1,21 @@
+# Interface gráfica com ícone e POO
+import sys
+import threading
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QIcon
+
+class janelaPrincipal(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        
+    def initUI(self):
+        self.setGeometry(0, 20, 1366, 748)
+        self.setWindowTitle('Janela com ícone')
+        self.setWindowIcon(QIcon('world.png'))
+        self.show()
+        
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = janelaPrincipal()
+    sys.exit(app.exec_()) 
